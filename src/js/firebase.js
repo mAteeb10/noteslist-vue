3051852be419495096281eeafa91b04fc23492c9
mAteeb10 <102751908+mAteeb10@users.noreply.github.com>
2024-06-22 14:20:43 +0500

    Practiced notes collection using VueJs wit Firebase service Firestore

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCgCsSurdcSSGp1Tuik8L9GqOE65eenISU',
-  authDomain: 'noteslist-vue.firebaseapp.com',
-  projectId: 'noteslist-vue',
-  storageBucket: 'noteslist-vue.appspot.com',
-  messagingSenderId: '692628756704',
-  appId: '1:692628756704:web:70ccaed79e28eea4ec1885'
+  apiKey: "AIzaSyAul-XjjApq9-y7EsnDGqC5Y9a7k7QBUks",
+  authDomain: "dashboard-for-data-entry.firebaseapp.com",
+  projectId: "dashboard-for-data-entry",
+  storageBucket: "dashboard-for-data-entry.appspot.com",
+  messagingSenderId: "278053965553",
+  appId: "1:278053965553:web:6526966505513a118e6350",
+  measurementId: "G-NGNPZ92TDC",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 
